323e4697bf66fd7761fa5a4127415c9b8731d0a2
o1-spec <oluwafemionadokun@gmail.com>
2024-07-02 14:23:28 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function updateTime() {
+    const currentTimeUTC = new Date().toUTCString();
+    document.querySelector('[data-testid="currentTimeUTC"]').textContent = currentTimeUTC;
+
+    const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+    document.querySelector('[data-testid="currentDay"]').textContent = currentDay;
+}
+
+window.onload = updateTime;
+setInterval(updateTime, 60000); // Update time every minute
